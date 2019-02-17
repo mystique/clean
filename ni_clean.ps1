6d78258f01963e0749c4700413b59cd0152a18ff
mystique <mystique@users.noreply.github.com>
2019-02-18 04:08:35 +0800

    Update ni_clean.ps1

--- ni_clean.ps1
+++ ni_clean.ps1
@@ -1,41 +1,106 @@
-$RegKey = @(
-    "HKCU:\Software\Native Instruments\Komplete Kontrol\",
-    "HKCU:\Software\Native Instruments\Kontakt 5\",
-    "HKCU:\Software\Native Instruments\Maschine 2\",
-    "HKCU:\Software\Native Instruments\Shared\",
-    "HKCU:\Software\Native Instruments\Absynth 5\",
-    "HKCU:\Software\Native Instruments\Battery 4\",
-    "HKCU:\Software\Native Instruments\Guitar Rig 5\",
-    "HKCU:\Software\Native Instruments\Reaktor 6\",
-    "HKCU:\Software\Native Instruments\FM8\",
-    "HKCU:\Software\Native Instruments\Massive\"
-)
+$CleanKeyValueMapExclude = @{
+    # Mounted devices clean
+    @("HKLM:\SYSTEM\MountedDevices\") = @(
+        "\DosDevices\C:",
+        "\DosDevices\D:",
+        "\DosDevices\E:",
+        "\DosDevices\F:",
+        "\DosDevices\G:",
+        "\DosDevices\H:",
+        "\DosDevices\I:",
+        "\DosDevices\J:",
+        "\DosDevices\K:",
+        "\DosDevices\L:"
+    )
+    # Native Instruments plugins clean
+    @(
+        "HKCU:\Software\Native Instruments\Komplete Kontrol\",
+        "HKCU:\Software\Native Instruments\Kontakt Application\",
+        "HKCU:\Software\Native Instruments\Maschine 2\",
+        "HKCU:\Software\Native Instruments\Shared\",
+        "HKCU:\Software\Native Instruments\Absynth 5\",
+        "HKCU:\Software\Native Instruments\Battery 4\",
+        "HKCU:\Software\Native Instruments\Guitar Rig 5\",
+        "HKCU:\Software\Native Instruments\Reaktor 6\",
+        "HKCU:\Software\Native Instruments\FM8\",
+        "HKCU:\Software\Native Instruments\Massive\"
+    ) = @(
+        "AB2",
+        "keyboardVisible",
+        "masterVisible",
+        "useKBForMIDI",
+        "browserLibsAZSort",
+        "OnStartupLoadProject",
+        "uret-allow-transfer",
+        "showNetworkDrivesInBrowser"
+    )
 
-$ExcludeValue = @(
-    "AB2",
-    "keyboardVisible",
-    "masterVisible",
-    "useKBForMIDI",
-    "browserLibsAZSort",
-    "OnStartupLoadProject",
-    "uret-allow-transfer"
-)
+}
 
-foreach ($Path in $RegKey) {
+$CleanKeyValueMapExcludeRecurse = @{
+    # FabFilter
+    @("HKCU:\Software\FabFilter") = @("LicenseText")
+}
 
-    function IsRemove ($Path) {
-        foreach ($ValueName in $ExcludeValue) {
-            if ($Path.StartsWith($ValueName)) {
-                return $False
-            }
+$CleanKeyValueMap = @{
+    # iZotope clean
+    @("HKCU:\Software\iZotope\RX7\") = @(
+        "ActiveClipName",
+        "ClipController",
+        "Last",
+        "Recent"
+    )
+}
+
+function IsExist($Path, $CheckValues, $Reverse = $False) {
+
+    foreach ($Value in $CheckValues) {
+        if ($Path.StartsWith($Value)) {
+            return !$Reverse
         }
-        return $True
     }
+    return $Reverse
+}
 
-    Get-Item -Path $Path | Select-Object -ExpandProperty Property | ForEach-Object {
-        if (IsRemove($_)) {
-            Write-Host "deleting: "$Path$_
-            Remove-ItemProperty -Path $Path -Name $_
+function CleanRegValues($RegKeys, $Reverse) {
+
+    foreach ($Paths in $RegKeys.Keys) {
+
+        foreach ($Path in $Paths) {
+
+            Get-Item -Path $Path | Select-Object -ExpandProperty Property | ForEach-Object {
+
+                if (IsExist $_ $RegKeys.Item($Paths) $Reverse) {
+                    Write-Host "deleting: "$Path$_
+                    Remove-ItemProperty -Path $Path -Name $_
+                }
+            }
         }
     }
 }
+
+function CleanRegValuesRecurse($RegKeys, $Reverse) {
+
+    foreach ($Paths in $RegKeys.Keys) {
+
+        foreach ($Path in $Paths) {
+
+            Get-ChildItem -Path $Path -Recurse | Select-Object | ForEach-Object {
+
+                foreach ($Property in $_.Property) {
+                    if (IsExist $Property $RegKeys.Item($Paths) $Reverse) {
+                        Write-Host "deleting: "$_.Name$Property
+                        Remove-ItemProperty -Path $_.PSPath -Name $Property
+                    }
+                }
+            }
+        }
+    }
+}
+
+CleanRegValues $CleanKeyValueMapExclude $True
+CleanRegValues $CleanKeyValueMap $False
+
+CleanRegValuesRecurse $CleanKeyValueMapExcludeRecurse $True
+
+Clear-History
